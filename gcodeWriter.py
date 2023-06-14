5594538dfdc655e02cb71030b2e06dca079ea50d
Michał Romańczyk <devromanczyk@gmail.com>
2023-06-14 21:56:36 +0200

    writer changes

--- gcodeWriter.py
+++ gcodeWriter.py
@@ -26,8 +26,8 @@ class GcodeWriter:
         'fan_off': 'M106 S0 '
     }
     new_line = '\n'
-    X_offset = 50 #in mm
-    Y_offset = 50 #in mm
+    X_offset = 25 #in mm
+    Y_offset = 25 #in mm
     gcode = """````
     Hi! This gcode was generated using image-2-gcode python script.
     Credits RomanczykMichal @ github.com
@@ -45,7 +45,7 @@ class GcodeWriter:
         self.gcode += self.new_line
 
     def move_to_point(self, point):
-        self.gcode += self.commands['positioning'] + self.commands['x'] + str(point['x'] + self.X_offset) + self.commands['y'] + str(point['y'] + self.Y_offset)
+        self.gcode += self.commands['positioning'] + self.commands['x'] + str(point['x'] + self.X_offset) + self.commands['y'] + str(point['y'] + self.Y_offset) + self.new_line
 
     def lift(self):
          self.gcode += self.commands['positioning'] + self.commands['z'] + '5' + self.new_line
@@ -69,4 +69,6 @@ class GcodeWriter:
         self.gcode += self.commands['home'] + self.new_line
 
     def __write_last_line(self):
+        self.lift()
+        self.move_to_point({"x": 0, "y": 0})
         self.gcode += self.commands['end_of_program']
